c83f6145d8b83005318cf606c77da8c11a9aec1e
csalvador <salvador.cedric@gmail.com>
2013-03-12 00:31:59 +0100

    Slightly better, could possibly work if I figure out proGuard. ~I have no idea what I'm doing~

--- cluster/project/Build.scala
+++ cluster/project/Build.scala
@@ -9,11 +9,11 @@ object General {
     version := "0.1",
     versionCode := 0,
     scalaVersion := "2.9.2",
-    platformName in Android := "android-4.2"
+    platformName in Android := "android-17"
   )
 
   val proguardSettings = Seq (
-    useProguard in Android := false
+    useProguard in Android := true
   )
 
   lazy val fullAndroidSettings =
